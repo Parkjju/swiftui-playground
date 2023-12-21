3aea2965fbac6f82c40ce580d5653c70447723bc
Parkjju <rudwns3927@gmail.com>
2023-12-21 15:06:28 +0900

    Initial Commit

---
+++ swiftUI-playground/swiftUI_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_playgroundApp.swift
+//  swiftUI-playground
+//
+//  Created by Jun on 2023/12/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
